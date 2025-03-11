3d4d9cf3cd5c8b4c3bbd86b9f82ce521baa35882
sohyunkim <alice3405@naver.com>
2025-03-11 18:20:14 +0900

    [add] 반응형

--- js/GSAP.js
+++ js/GSAP.js
@@ -1,19 +1,88 @@
 document.addEventListener("DOMContentLoaded", (event) => {
   gsap.registerPlugin(ScrollTrigger);
 
+  // 🥦🥦 인트로 빨간 박스 올라감
   gsap
     .timeline({ delay: 1 })
     .to(".blind_top", { height: "0" })
     .to(".blind_bottom", { height: "0" }, "<");
 
+  // 🥦🥦 메뉴 탭 나타내기
   gsap
     .timeline({
       scrollTrigger: {
         trigger: ".introduce",
+        start: "-300px, 0%",
+        end: "100%, 100%",
+        scrub: true,
+      },
+    })
+    .to(".menu_tab", { right: "0" });
+
+  // 🥦🥦 영역별 메뉴 글자색 변경
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".introduce",
+        start: "-300px, 0%",
+        end: "100%, 100%",
+        scrub: true,
+      },
+    })
+    .to(".menu1 a", { color: "#c8000a", fontWeight: "800" })
+    .to(".menu2 a", { color: "#eeeeee", fontWeight: "300" })
+    .to(".menu3 a", { color: "#eeeeee", fontWeight: "300" });
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".work",
         start: "0% 0%",
         end: "100%, 100%",
         scrub: true,
       },
     })
-    .fromTo(".menu_tab", { x: "300px" }, { x: "0" });
+    .to(".menu1 a", { color: "#eeeeee", fontWeight: "300" })
+    .to(".menu2 a", { color: "#29a5fe", fontWeight: "800" })
+    .to(".menu3 a", { color: "#eeeeee", fontWeight: "300" });
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".contact",
+        start: "-10% 0%",
+        end: "100%, 100%",
+        scrub: true,
+      },
+    })
+    .to(".menu1 a", { color: "#eeeeee", fontWeight: "300" })
+    .to(".menu2 a", { color: "#eeeeee", fontWeight: "300" })
+    .to(".menu3 a", { color: "#91ea91", fontWeight: "800" });
+
+  gsap
+    .timeline({
+      scrollTrigger: {
+        trigger: ".introduce",
+        start: "top 10%", // 요소가 화면 아래쪽 80% 지점에서 시작
+        end: "bottom top", // 요소가 화면 밖으로 나갈 때까지 애니메이션 적용
+        scrub: true, // 스크롤에 따라 부드럽게 진행
+        pin: true, // 화면 고정 없이 자연스럽게 이동
+        markers: true, // 디버깅용 (완성 후 삭제 가능)
+      },
+    })
+    .fromTo(
+      ".card1",
+      { y: -600, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1, ease: "power2.out" }
+    )
+    .fromTo(
+      ".card2",
+      { y: -600, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1, ease: "power2.out" }
+    )
+    .fromTo(
+      ".card3",
+      { y: -600, opacity: 0 },
+      { y: 0, opacity: 1, duration: 1, ease: "power2.out" }
+    );
 });
--- js/main.js
+++ js/main.js
@@ -12,3 +12,24 @@ function raf(time) {
   requestAnimationFrame(raf);
 }
 requestAnimationFrame(raf);
+
+// 타이핑 애니메이션 ------------------------------
+const content = "- 김소현, 2025, 퍼블리셔 포트폴리오.";
+const text = document.querySelector(".typing_box p");
+let i = 0;
+
+function typing() {
+  if (i < content.length) {
+    text.innerHTML += content[i++];
+    setTimeout(typing, 140); // 다음 글자 출력
+  }
+  cusor.style.display = "none";
+}
+setTimeout(typing, 3000);
+
+// 타이핑 커서 숨기기
+function cusorNone() {
+  const cusor = document.querySelector(".typing_box .blink");
+  cusor.style.visibility = "hidden";
+}
+setTimeout(cusorNone, 7000);
